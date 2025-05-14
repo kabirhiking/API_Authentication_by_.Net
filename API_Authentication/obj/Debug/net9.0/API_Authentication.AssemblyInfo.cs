82e25de20b6b739aa42700ff801355ccbe360c3e
kabir <kabirraihan249@gmail.com>
2025-05-15 02:13:49 +0600

    add readme file

--- API_Authentication/obj/Debug/net9.0/API_Authentication.AssemblyInfo.cs
+++ API_Authentication/obj/Debug/net9.0/API_Authentication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Authentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b5fc93f906c6856c8997b17871a2f8e745dc22")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Authentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Authentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
